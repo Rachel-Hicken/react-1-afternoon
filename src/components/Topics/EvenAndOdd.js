6d04d91f86dbc5738a94ebb9506e137129494ab3
Rachel Hicken <rachkh22@gmail.com>
2018-04-21 22:43:42 -0600

    Beginning step three

--- src/components/Topics/EvenAndOdd.js
+++ src/components/Topics/EvenAndOdd.js
@@ -1,9 +1,43 @@
 import React, {Component} from 'react';
 
 class EvenAndOdd extends Component{
+    constructor(){
+        super()
+        this.state={
+            evenArray: [],
+            oddArray: [],
+            userInput: ''
+        }
+    }
+
+    changeHandler(value){
+        this.setState({userInput: value})
+    }
+
+    evenOddArrs(userInput){
+        let arr = userInput.split(',');
+        let evens = [];
+        let odds = [];
+        for(var i=0;i<userInput.length;i++){
+            if(arr[i]%2===0){
+                evens.push(arr[i]);
+            }
+            else{
+                odds.push(arr[i]);
+            }
+        }
+    }
+
     render(){
         return(
-            <p>EvenAndOdd</p>
+            <div className='puzzleBox evenAndOddPB'>
+                <h4>Evens and Odds</h4>
+                <input onChange={(event)=>this.state.userInput(event)} className='inputLine'/>
+                <button className='confirmationButton'></button>
+                <span className='resultsBox'></span>
+                <span className='resultsBox'></span>
+            </div>
+
         )
     }
 }
